919d7cdc9ccd8357cb88a7514ce276812f12f8b9
A-athul-A <athul.lee@gmail.com>
2022-12-01 00:05:57 +0530

    commit

---
+++ shoppingcartProject/shoppingApp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'shoppingApp'
+urlpatterns = [
+
+    path('',views.allProCat,name='allProCat'),
+    path('<slug:c_slug>/',views.allProCat,name="products_by_category"),
+    path('<slug:c_slug>/<slug:product_slug>/',views.proDetail,name="prodCatDetail"),
+
+]
